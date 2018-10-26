f8b07a99b11305be6e10d63f6542da340d9c6576
Benedek László <lacbenedek@gmail.com>
2018-10-26 18:49:00 +0200

    added timer

---
+++ Assets/Scripts/Game/FinishLine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+    private StartLine startLine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            startLine = FindObjectOfType<StartLine>();
+
+            startLine.counting = false;
+
+            startLine.Draw();
+            startLine.time = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/StartLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartLine : MonoBehaviour {
+
+    public bool counting = false;
+    public float time = 0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+            counting = true;
+    }
+
+    private void Update()
+    {
+        if (counting)
+            time += Time.deltaTime;
+    }
+
+    public void Draw()
+    {
+        GameObject.Find("Timer").GetComponent<Text>().text = time.ToString() + " s";
+    }
+}
